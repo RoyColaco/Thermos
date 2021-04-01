84f526cf7055ebc0406d83ab5023590d44c105ec
sysadmin <sysadmin@bcalab1-m23>
2021-04-01 09:49:17 +0530

    hello

---
+++ src/apple/thermos.java
@@ -0,0 +1,29 @@
+package apple;
+
+public class thermos {
+	float liters;
+	String brand;
+	String Liquid;
+	
+	thermos(String x,String y)
+	{
+		liters=0;
+		brand=x;
+		Liquid=y;
+	}
+	
+	void addLiq(float x)
+	{
+		liters=liters+x;
+	}
+	
+	void details()
+	{
+		System.out.println("Brand : "+brand+"\nLiquid : "+Liquid+"Liter : "+liters);
+	}
+	
+	public static void main(String args[]) {
+
+		
+	}
+}
